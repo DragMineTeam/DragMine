718828303a56759a322125303feb84776b48834f
towa0131 <towa.towa0131@icloud.com>
2018-01-07 23:55:34 +0900

    Deleted message

--- src/raklib/server/Session.php
+++ src/raklib/server/Session.php
@@ -413,7 +413,6 @@ class Session{
 					$dataPacket->decode();
 
 					if($dataPacket->port === $this->sessionManager->getPort() or !$this->sessionManager->portChecking){
-						echo "Connected from " . $dataPacket->address . " | " . $dataPacket->port;
 						$this->state = self::STATE_CONNECTED; //FINALLY!
 						$this->isTemporal = false;
 						$this->sessionManager->openSession($this);
