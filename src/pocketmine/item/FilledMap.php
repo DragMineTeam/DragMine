df53dd5c59e9bd0703f760526c2f63d7b13ec15e
towa0131 <towa.towa0131@icloud.com>
2018-06-13 18:10:24 +0900

    Added ASCII logo to some files

--- src/pocketmine/item/FilledMap.php
+++ src/pocketmine/item/FilledMap.php
@@ -1,5 +1,24 @@
 <?php
 
+/*
+ *
+ *  ____                  __  __ _            
+ * |  _ \ _ __ __ _  __ _|  \/  (_)_ __   ___ 
+ * | | | | '__/ _` |/ _` | |\/| | | '_ \ / _ \
+ * | |_| | | | (_| | (_| | |  | | | | | |  __/
+ * |____/|_|  \__,_|\__, |_|  |_|_|_| |_|\___|
+ *                  |___/                     
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author DragMine Team
+ *
+ *
+*/
+
 namespace pocketmine\item;
 
 use pocketmine\item\Item;
